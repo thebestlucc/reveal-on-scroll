fe8aea38d65a5837d8d1f5e83f18210e9851a114
thebestlucc <thebestlucc@gmail.com>
2023-10-31 11:35:12 -0300

    chore(vite.config): adjust assets path generation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+function transformHtml() {
+  return {
+    name: "change assets path",
+    transformIndexHtml(html) {
+      return html.replaceAll("/assets", "/reveal-on-scroll/assets");
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), transformHtml()],
+});
